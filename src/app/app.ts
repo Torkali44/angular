b6c2400a47522de0ba70fd3de6b0bef41a15c312
Torkali44 <torkelzohry@gmail.com>
2026-03-27 15:12:54 +0200

    filtering product and Bootstrap cards

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CoursesComponent } from "./courses/courses";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CoursesComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
